fe99da072e9001e778c5537ea3f9c26c27455ef9
muhammad613 <muhammad2808.a@mail.ru>
2024-03-10 05:01:59 +0600

    edit settings

--- config/settings.py
+++ config/settings.py
@@ -18,10 +18,9 @@ CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# CSRF_TRUSTED_ORIGINS = [
-#     'https://www.<domain.com>',
-#     'http://www.<domain.com>'
-# ]
+CSRF_TRUSTED_ORIGINS = [
+    'https://crynox.tech',
+]
 
 MY_APPS = [
     'apps.monitor',
