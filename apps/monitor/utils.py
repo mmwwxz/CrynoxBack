60b402c77c6f938fa37dc33f6118c175a4d70de2
muhammad613 <muhammad2808.a@mail.ru>
2024-03-10 06:51:14 +0600

    edit sending email

--- apps/monitor/utils.py
+++ apps/monitor/utils.py
@@ -4,15 +4,19 @@ from django.contrib.auth.models import User
 
 def send_support_completion_email(lead, is_admin=False):
     subject = 'Поддержка продукта завершена'
-    if not is_admin:
-        message = (f'Уважаемый {lead.name},\n\nПоддержка вашего продукта завершена. '
-                   f'Если у вас возникнут вопросы, пожалуйста, свяжитесь с нами.\n\n'
-                   f'С уважением,\nВаша команда поддержки')
-    else:
-        message = (f'Уважаемый администратор,\n\nПоддержка продукта пользователя {lead.name} завершена. '
-                   f'Пожалуйста, примите соответствующие меры.\n\n'
-                   f'С уважением,\nВаша команда поддержки')
+    from_email = 'crynox.devtes@gmail.com'
 
-    from_email = 'from@example.com'
-    to_email = [user.email for user in User.objects.filter(is_staff=False)]
+    if not is_admin:
+        message = (f'Уважаемый {lead.name},\n\n'
+                   'Поддержка вашего продукта завершена. '
+                   'Если у вас возникнут вопросы, пожалуйста, свяжитесь с нами.\n\n'
+                   'С уважением,\nВаша команда поддержки')
+        to_email = [lead.email]
+    else:
+        message = (f'Уважаемый администратор,\n\n'
+                   f'Поддержка продукта пользователя {lead.name} завершена. '
+                   'Пожалуйста, примите соответствующие меры.\n\n'
+                   'С уважением,\nВаша команда поддержки')
+        to_email = [user.email for user in User.objects.filter(is_staff=True)]
+
     send_mail(subject, message, from_email, to_email, fail_silently=False)
