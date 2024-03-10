fd9db763af47378dbe12c07515bccfb8f909462a
muhammad613 <muhammad2808.a@mail.ru>
2024-03-10 07:07:02 +0600

    edit sending email

--- apps/monitor/utils.py
+++ apps/monitor/utils.py
@@ -14,9 +14,11 @@ def send_support_completion_email(lead, is_admin=False):
         to_email = [lead.email]
     else:
         message = (f'Уважаемый администратор,\n\n'
-                   f'Поддержка продукта пользователя {lead.name} завершена. '
-                   'Пожалуйста, примите соответствующие меры.\n\n'
-                   'С уважением,\nВаша команда поддержки')
+                   f'Поддержка продукта пользователя {lead.name} завершена.\n\n'
+                   f'ФИО: {lead.name}\n'
+                   f'Почта: {lead.email}\n'
+                   f'Номер: {lead.phone}\n\n'
+                   'Пожалуйста, примите соответствующие меры.\n\n')
         to_email = [user.email for user in User.objects.filter(is_staff=True)]
 
     send_mail(subject, message, from_email, to_email, fail_silently=False)
