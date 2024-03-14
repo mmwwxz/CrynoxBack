1b5205a7e94ec12cd75f7dcf5d23b1d4ebd70439
muhammad613 <muhammad2808.a@mail.ru>
2024-03-14 10:36:19 +0600

    dsa

--- apps/monitor/utils.py
+++ apps/monitor/utils.py
@@ -8,7 +8,7 @@ from apps.monitor.models import LeadSupport
 def send_support_completion_email(lead, is_admin=False):
     subject = 'Поддержка продукта завершена'
     from_email = 'crynox.devtes@gmail.com'
-    five_days_later = timezone.now() + timedelta(seconds=59)
+    five_days_later = timezone.now() + timedelta(minutes=2)
 
     lead_support = LeadSupport.objects.filter(lead=lead).latest('updating')
 
@@ -17,6 +17,7 @@ def send_support_completion_email(lead, is_admin=False):
             message = (
                 f'Уважаемый {lead.name},\n\n'
                 f'Поддержка вашего продукта закончится через 5 дней.\n'
+                f'Дата окончания поддержки: {lead_support.updating}\n'
                 f'Если у вас возникнут вопросы или вы хотите продлить поддержку вашего продукта, пожалуйста, свяжитесь с нами.\n\n'
                 f'С уважением,\nВаша команда поддержки CRYNOX'
             )
@@ -33,14 +34,28 @@ def send_support_completion_email(lead, is_admin=False):
             send_mail(subject, message, from_email, to_email, fail_silently=False)
 
     elif is_admin:
-        message = (
-            f'Уважаемый администратор,\n'
-            f'Поддержка продукта пользователя {lead.name} завершена.\n\n'
-            f'ФИО: {lead.name}\n'
-            f'Почта: {lead.email}\n'
-            f'Номер: {lead.phone}\n\n'
-            f'Дата окончания поддержки: {lead_support.updating}'
-            'Пожалуйста, примите соответствующие меры'
-        )
-        to_email = [user.email for user in User.objects.filter(is_staff=True)]
-        send_mail(subject, message, from_email, to_email, fail_silently=False)
+        if lead_support.updating == five_days_later.date():
+            message = (
+                f'Уважаемый администратор,\n'
+                f'Поддержка продукта пользователя {lead.name} закончится через 5 дней.\n\n'
+                f'ФИО: {lead.name}\n'
+                f'Почта: {lead.email}\n'
+                f'Номер: {lead.phone}\n\n'
+                f'Дата окончания поддержки: {lead_support.updating}'
+                'Пожалуйста, примите соответствующие меры'
+            )
+            to_email = [user.email for user in User.objects.filter(is_staff=True)]
+            send_mail(subject, message, from_email, to_email, fail_silently=False)
+
+        else:
+            message = (
+                f'Уважаемый администратор,\n'
+                f'Поддержка продукта пользователя {lead.name} завершена.\n\n'
+                f'ФИО: {lead.name}\n'
+                f'Почта: {lead.email}\n'
+                f'Номер: {lead.phone}\n\n'
+                f'Дата окончания поддержки: {lead_support.updating}'
+                'Пожалуйста, примите соответствующие меры'
+            )
+            to_email = [user.email for user in User.objects.filter(is_staff=True)]
+            send_mail(subject, message, from_email, to_email, fail_silently=False)
