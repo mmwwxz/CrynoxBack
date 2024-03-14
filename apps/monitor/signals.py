adfdd41c78e7c0f4854c243d58242aaf14a38d60
muhammad613 <muhammad2808.a@mail.ru>
2024-03-14 13:28:12 +0600

    dsa

--- apps/monitor/signals.py
+++ apps/monitor/signals.py
@@ -3,13 +3,16 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 from .models import LeadSupport
 from .utils import send_support_completion_email
+from datetime import timedelta
 
 
 @receiver(post_save, sender=LeadSupport)
 def send_support_completion_notification(sender, instance, created, **kwargs):
     if created:
         lead = instance.lead
-        today = timezone.now().date()
-        if instance.testing.date() <= today and instance.updating.date() <= today:
+        now = timezone.now()
+
+        if instance.updating <= now + timedelta(minutes=5) and instance.updating > now:
             send_support_completion_email(lead)
+        if instance.updating < now:
             send_support_completion_email(lead, is_admin=True)
